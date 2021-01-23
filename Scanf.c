404175f618d1140addbdd18f68b14b05f11ac19e
Arghya-992 <arghyadc2020@gmail.com>
2021-01-23 10:08:46 +0530

    This is my first c program

---
+++ Scanf.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+void main(void)
+{
+int i1,i2,sum;
+
+printf("Enter the value of i1 and i2 ");
+
+ scanf(" %d %d ",&i1,&i2);
+ 
+ sum = i1+i2;
+ 
+ printf ("The sum of %d and %d is %d ", i1,i2,sum);
+}
+ 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
