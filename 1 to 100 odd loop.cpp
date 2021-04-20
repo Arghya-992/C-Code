fc85134f3a1c5329e5d4b094d15b8e893c08b6b3
Arghya-992 <76256630+Arghya-992@users.noreply.github.com>
2021-04-20 19:28:21 +0530

    Add files via upload

---
+++ 1 to 100 odd loop.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	printf("\n This is a program only to show even numbers within 1 to 100");
+	
+	for(int i=0; i<100;i++){
+		if (i%2 != 0){
+	
+		printf("\n %d is an even number within 1 to 100",i);
+	}
+	}	
+}
